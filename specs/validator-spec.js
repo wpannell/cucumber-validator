478f252b03371510635acd9c4145adb3a8adc5bd
wpannell <wil.pannell@gmail.com>
2015-04-29 21:55:23 -0700

    act — middle of arrange/act/assert

--- specs/validator-spec.js
+++ specs/validator-spec.js
@@ -3,6 +3,7 @@
 
   describe('A Validator function will', function () {
     it('return error.nonpositive for not strictly positive numbers', function () {
+      var validator = new Validator();
       assert.deepEqual(validator(0), ['error.nonpositive']);
     });
   });
