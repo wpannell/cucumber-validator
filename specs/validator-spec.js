5dc36e3c4f4646dd5ed25f8c7e282cf6f5bf5856
wpannell <wil.pannell@gmail.com>
2015-04-29 21:58:39 -0700

    change object to function

--- specs/validator-spec.js
+++ specs/validator-spec.js
@@ -2,8 +2,10 @@
   var assert = require('assert');
 
   describe('A Validator function will', function () {
+
+    function validator() {};
+
     it('return error.nonpositive for not strictly positive numbers', function () {
-      var validator = new Validator();
       assert.deepEqual(validator(0), ['error.nonpositive']);
     });
   });
