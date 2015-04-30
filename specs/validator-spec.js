d6bceabfb3dc273caa6fb388a6fb87206bb30ee1
wpannell <wil.pannell@gmail.com>
2015-04-30 05:38:42 -0700

    refactor tests

--- specs/validator-spec.js
+++ specs/validator-spec.js
@@ -5,42 +5,42 @@
   describe('A Validator function will', function () {
     var validator = require('../lib/validator');
 
-    it('return no errors for valid numbers', function () {
+    it('include no errors for valid numbers', function () {
       expect(validator(7)).to.be.empty;
     });
 
-    describe('return error.nonpositive', function () {
+    describe('include error.nonpositive', function () {
       it('for 0', function () {
-        expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+        expect(validator(0)).to.include('error.nonpositive');
       });
 
       it('for negative', function () {
-        expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+        expect(validator(-2)).to.include('error.nonpositive');
       });
     });
 
-    describe('return error.three for mod 3 like', function () {
+    describe('include error.three for mod 3 like', function () {
       it('3', function () {
-        expect(validator(3)).to.be.deep.equal(['error.three']);
+        expect(validator(3)).to.include('error.three');
       });
 
       it('and 42', function () {
-        expect(validator(42)).to.be.deep.equal(['error.three']);
+        expect(validator(42)).to.include('error.three');
       });
     });
 
-    describe('return error.five for mod 5 like:', function () {
+    describe('include error.five for mod 5 like:', function () {
       it('5', function () {
-        expect(validator(5)).to.be.deep.equal(['error.five']);
+        expect(validator(5)).to.include('error.five');
       });
 
       it('and 10', function () {
-        expect(validator(10)).to.be.deep.equal(['error.five']);
+        expect(validator(10)).to.include('error.five');
       });
     });
 
-    it('will return one error for each violation', function () {
-      expect(validator(15)).to.be.deep.equal(['error.three', 'error.five']);
+    it('will include one error for each violation', function () {
+      expect(validator(15)).to.include('error.three', 'error.five');
     });
   });
 })();
