9a4f11219c0825a2fdc86a9daf843df062659309
wpannell <wil.pannell@gmail.com>
2015-04-29 23:40:28 -0700

    green

--- specs/validator-spec.js
+++ specs/validator-spec.js
@@ -6,7 +6,7 @@
     var validator = require('../lib/validator');
 
     it('return no errors for valid numbers', function () {
-      expect(validator(3)).to.be.empty;
+      expect(validator(7)).to.be.empty;
     });
 
     describe('return error.nonpositive', function () {
@@ -21,7 +21,7 @@
 
     describe('return error.three for mod 3 like', function () {
       it('3', function () {
-        expect(validator(0)).to.be.deep.equal(['error.three']);
+        expect(validator(3)).to.be.deep.equal(['error.three']);
       });
     });
 
