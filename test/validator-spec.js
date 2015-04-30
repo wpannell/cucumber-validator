6a7f5feb446258662896d5a8c2b2c35f47dcc59a
wpannell <wil.pannell@gmail.com>
2015-04-29 21:39:09 -0700

    add failing test

---
+++ test/validator-spec.js
@@ -0,0 +1,9 @@
+(function(){
+  var assert = require('assert');
+
+  describe('Be welcome to Mocha', function() {
+    it('with a failing test', function() {
+      assert(false, 'Hello World');
+    });
+  });
+})();
